5482b09f18321b99f58ca44d5f4d04bf70133c04
Riya020103 <riyasharmma020103@gmail.com>
2024-07-30 23:42:24 +0530

    Create MVCPatternExample.java

---
+++ Assignment 1/Task 2/Question 10/MVCPatternExample.java
@@ -0,0 +1,104 @@
+class Student {
+
+  private String name;
+  private int id;
+  private String grade;
+
+  public Student(String name, int id, String grade) {
+    this.name = name;
+    this.id = id;
+    this.grade = grade;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getGrade() {
+    return grade;
+  }
+
+  public void setGrade(String grade) {
+    this.grade = grade;
+  }
+}
+
+class StudentView {
+
+  public void displayStudentDetails(String name, int id, String grade) {
+    System.out.println("Student Details:");
+    System.out.println("Name: " + name);
+    System.out.println("ID: " + id);
+    System.out.println("Grade: " + grade);
+  }
+}
+
+class StudentController {
+
+  private Student model;
+  private StudentView view;
+
+  public StudentController(Student model, StudentView view) {
+    this.model = model;
+    this.view = view;
+  }
+
+  public void setStudentName(String name) {
+    model.setName(name);
+  }
+
+  public String getStudentName() {
+    return model.getName();
+  }
+
+  public void setStudentId(int id) {
+    model.setId(id);
+  }
+
+  public int getStudentId() {
+    return model.getId();
+  }
+
+  public void setStudentGrade(String grade) {
+    model.setGrade(grade);
+  }
+
+  public String getStudentGrade() {
+    return model.getGrade();
+  }
+
+  public void updateView() {
+    view.displayStudentDetails(
+      model.getName(),
+      model.getId(),
+      model.getGrade()
+    );
+  }
+}
+
+public class MVCPatternExample {
+
+  public static void main(String[] args) {
+    Student model = new Student("John Doe", 1, "A");
+    StudentView view = new StudentView();
+    StudentController controller = new StudentController(model, view);
+
+    controller.updateView();
+    controller.setStudentName("Jane Smith");
+    controller.setStudentId(2);
+    controller.setStudentGrade("B");
+    controller.updateView();
+  }
+}
